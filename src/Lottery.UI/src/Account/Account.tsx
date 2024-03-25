6b11ccab9f5a285773709fce5f3166a7b698d866
devklick <devklick@gmail.com>
2024-03-25 08:26:30 +0000

    Navigate to sign in if not authed when accessing home

--- src/Lottery.UI/src/Account/Account.tsx
+++ src/Lottery.UI/src/Account/Account.tsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useUserStore } from "../stores/user.store";
+import { useEffect } from "react";
+
 interface AccountProps {}
 
 function Account({}: AccountProps) {
+  const user = useUserStore();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user.authenticated()) {
+      navigate("/account/signIn");
+    }
+  });
   return "Hello from account";
 }
 
